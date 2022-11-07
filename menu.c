ec24ee4a8724eedeebfd7119abd209b6a47c8f5a
agatheeber <92367538+agatheeber@users.noreply.github.com>
2022-11-07 15:06:48 +0100

    nouveau menu

--- menu.c
+++ menu.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "tp3.h"
+
 
 
 int main() {
@@ -8,33 +10,37 @@ int main() {
     for (int i=0;i<4;i++){
         listemat[i]=NULL;
     }
-    matrice_creuse* m0;
-    matrice_creuse* m1;
-    matrice_creuse* m2;
-    matrice_creuse* m3;
+
+    matrice_creuse* m0= malloc(sizeof(matrice_creuse));
+    matrice_creuse* m1=malloc(sizeof(matrice_creuse));
+    matrice_creuse* m2=malloc(sizeof(matrice_creuse));
+    matrice_creuse* m3=malloc(sizeof(matrice_creuse));
+
     while (continu==1){  //affichage menu
-        printf("1. Remplir une matrice creuse\n");
-        printf("2. Afficher une matrice creuse sous forme de tableau\n");
-        printf("3. Afficher une matrice creuse sous forme de listes\n");
-        printf("4. Donner la valeur d’un élément d’une matrice creuse\n");
-        printf("5. Affecter une valeur à un élément d’une matrice creuse\n");
-        printf("6. Additionner deux matrices creuses\n");
-        printf("7. Calculer le gain en espace en utilisant cette représentation pour une matrice donnée\n");
-        printf("8. Quitter\n");
-        printf("saisir une option : 1 pour 1., 2 pour 2, ...");
+        printf("\n1. Remplir une matrice creuse");
+        printf("\n2. Afficher une matrice creuse sous forme de tableau");
+        printf("\n3. Afficher une matrice creuse sous forme de listes");
+        printf("\n4. Donner la valeur d’un élément d’une matrice creuse");
+        printf("\n5. Affecter une valeur à un élément d’une matrice creuse");
+        printf("\n6. Additionner deux matrices creuses");
+        printf("\n7. Calculer le gain en espace en utilisant cette représentation pour une matrice donnée");
+        printf("\n8. Quitter");
+        printf("\nSaisir une option : ");
         int x;
         scanf("%d",&x); //sélection du menu
-        system("cls"); //effacage de l'écran par soucis de lisibilité
+        
         switch(x){
-        case 1:
-                printf(" saisir un nombre de lignes");
+            case 1:
+                printf(" saisir un nombre de lignes : ");
                 int N;
                 scanf("%d",&N);
-                printf(" saisir un nombre de colonnes");
+                printf(" saisir un nombre de colonnes : ");
                 int M;
                 scanf("%d",&M);
+
                 if (listemat[0]==NULL){
                     listemat[0]="m0";
+
                     remplirMatrice(m0,N,M);
                     printf("la matrice disponible créées est %s", listemat[0]);
                     break;
@@ -60,173 +66,196 @@ int main() {
                 break;
 
             case 2:
-                    printf("saisir le numéro de la matrice à afficher (les matrices disponibles sont %s %s %s %s)", listemat[0], listemat[1],listemat[2], listemat[3]);
-                    int a;
-                    scanf("%d",&a);
-                    switch(a){
-                        case 0: afficherMatrice(*m0);break;
-                        case 1: afficherMatrice(*m1);break;
-                        case 2: afficherMatrice(*m2);break;
-                        case 3: afficherMatrice(*m3);break;
-                        default:
-                            printf("saisie incorrecte");
-                            break;
-                    }
-                    break;
+                printf("saisir le numéro de la matrice à afficher (les matrices disponibles sont %s %s %s %s)", listemat[0], listemat[1],listemat[2], listemat[3]);
+                int a;
+                scanf("%d",&a);
+                switch(a){
+                    case 0: afficherMatrice(*m0);break;
+                    case 1: afficherMatrice(*m1);break;
+                    case 2: afficherMatrice(*m2);break;
+                    case 3: afficherMatrice(*m3);break;
+                    default:
+                        printf("saisie incorrecte");
+                        break;
+                }
+                break;
 
             case 3:
-                    printf("saisir le numéro de la matrice à afficher (les matrices disponibles sont %s %s %s %s)", listemat[0], listemat[1],listemat[2], listemat[3]);
-                    int b;
-                    scanf("%d",&b);
-                    switch(b){
-                        case 0: afficherMatriceListes(*m0);break;
-                        case 1: afficherMatriceListes(*m1);break;
-                        case 2: afficherMatriceListes(*m2);break;
-                        case 3: afficherMatriceListes(*m3);break;
-                        default:
-                            printf("saisie incorrecte");
-                            break;
-                    }
-                    break;
+                printf("Saisir le numéro de la matrice à afficher (les matrices disponibles sont %s %s %s %s)", listemat[0], listemat[1],listemat[2], listemat[3]);
+                int b;
+                scanf("%d",&b);
+                switch(b){
+                    case 0: afficherMatriceListes(*m0);break;
+                    case 1: afficherMatriceListes(*m1);break;
+                    case 2: afficherMatriceListes(*m2);break;
+                    case 3: afficherMatriceListes(*m3);break;
+                    default:
+                        printf("saisie incorrecte");
+                        break;
+                }
+                break;
 
             case 4:
-                    printf("saisir le numéro de la matrice dont on souhaite afficher la valeur d'un coefficient(les matrices disponibles sont %s %s %s %s)", listemat[0], listemat[1],listemat[2], listemat[3]);
-                    int c;
-                    scanf("%d",&c);
-                    int i;
-                    int j;
-                    printf("saisir le numéro de la ligne du coefficient à afficher");
-                    scanf("%d",&i);
-                    printf("saisir le numéro de la colonne du coefficient à afficher");
-                    scanf("%d",&j);
-                    switch(c){
-                        case 0 : printf("la valeur du coefficient %d-%d est %d",i,j,rechercherValeur(*m0,i,j));break;
-                        case 1 : printf("la valeur du coefficient %d-%d est %d",i,j,rechercherValeur(*m0,i,j));break;
-                        case 2 :printf("la valeur du coefficient %d-%d est %d",i,j,rechercherValeur(*m0,i,j));break;
-                        case 3 :printf("la valeur du coefficient %d-%d est %d",i,j,rechercherValeur(*m0,i,j));break;
-                        default:printf("saisie incorrecte");break;
-                    }
-                    break;
+                printf("saisir le numéro de la matrice dont on souhaite afficher la valeur d'un coefficient(les matrices disponibles sont %s %s %s %s)", listemat[0], listemat[1],listemat[2], listemat[3]);
+                int c;
+                scanf("%d",&c);
+                int i;
+                int j;
+                printf("Saisir le numéro de la ligne du coefficient à afficher");
+                scanf("%d",&i);
+                printf("Saisir le numéro de la colonne du coefficient à afficher");
+                scanf("%d",&j);
+                switch(c){
+                    case 0 : printf("La valeur du coefficient %d-%d est %d",i,j,rechercherValeur(*m0,i,j));break;
+                    case 1 : printf("La valeur du coefficient %d-%d est %d",i,j,rechercherValeur(*m0,i,j));break;
+                    case 2 :printf("La valeur du coefficient %d-%d est %d",i,j,rechercherValeur(*m0,i,j));break;
+                    case 3 :printf("La valeur du coefficient %d-%d est %d",i,j,rechercherValeur(*m0,i,j));break;
+                    default:printf("Saisie incorrecte");break;
+                }
+                break;
 
             case 5:
-                    printf("saisir le numéro de la matrice à laquelle on souhaite affecter la valeur d'un coefficient(les matrices disponibles sont %s %s %s %s)", listemat[0], listemat[1],listemat[2], listemat[3]);
-                    int d;
-                    scanf("%d",&d);
-                    int k;
-                    int l;
-                    int val;
-                    printf("saisir la valeur que l'on souhaite affecter");
-                    scanf("%d",&val);
-                    printf("saisir le numéro de la ligne du coefficient à modifier");
-                    scanf("%d",&k);
-                    printf("saisir le numéro de la colonne du coefficient à modifier");
-                    scanf("%d",&l);
-                    switch(d){
-                        case 0 :
-                            affecterValeur(*m0,k,l,val);
-                            printf("la valeur du coefficient %d-%d est désormais %d",k,l,val);
-                            break;
+                printf("Saisir le numéro de la matrice à laquelle on souhaite affecter la valeur d'un coefficient(les matrices disponibles sont %s %s %s %s)", listemat[0], listemat[1],listemat[2], listemat[3]);
+                int d;
+                scanf("%d",&d);
+                int k;
+                int l;
+                int val;
+                printf("Saisir la valeur que l'on souhaite affecter");
+                scanf("%d",&val);
+                printf("Saisir le numéro de la ligne du coefficient à modifier");
+                scanf("%d",&k);
+                printf("Saisir le numéro de la colonne du coefficient à modifier");
+                scanf("%d",&l);
+                switch(d){
+                    case 0 :
+                        affecterValeur(*m0,k,l,val);
+                        printf("La valeur du coefficient %d-%d est désormais %d",k,l,val);
+                        break;
 
-                        case 1 :
-                            affecterValeur(*m1,k,l,val);
-                            printf("la valeur du coefficient %d-%d est désormais %d",k,l,val);
-                            break;
+                    case 1 :
+                        affecterValeur(*m1,k,l,val);
+                        printf("La valeur du coefficient %d-%d est désormais %d",k,l,val);
+                        break;
 
-                        case 2 :
-                            affecterValeur(*m2,k,l,val);
-                            printf("la valeur du coefficient %d-%d est désormais %d",k,l,val);
-                            break;
+                    case 2 :
+                        affecterValeur(*m2,k,l,val);
+                        printf("La valeur du coefficient %d-%d est désormais %d",k,l,val);
+                        break;
 
-                        case 3 :
-                            affecterValeur(*m3,k,l,val);
-                            printf("la valeur du coefficient %d-%d est désormais %d",k,l,val);
-                            break;
+                    case 3 :
+                        affecterValeur(*m3,k,l,val);
+                        printf("La valeur du coefficient %d-%d est désormais %d",k,l,val);
+                        break;
 
-                        default:
-                            printf("saisie incorrecte");
-                            break;
-                    }
-                    break;
+                    default:
+                        printf("Saisie incorrecte");
+                        break;
+                }
+                break;
 
             case 6 :
-                printf("saisir le numéro de la première matrice à additionner(les matrices disponibles sont %s %s %s %s)", listemat[0], listemat[1],listemat[2], listemat[3]);
+                printf("Saisir le numéro de la première matrice à additionner(les matrices disponibles sont %s %s %s %s)", listemat[0], listemat[1],listemat[2], listemat[3]);
                 int c1; //choix 1
                 int c2; //choix 2
                 scanf("%d",&c1);
+
+                printf("Saisir le numéro de deuxième matrice à additionner(les matrices disponibles sont %s %s %s %s)", listemat[0], listemat[1],listemat[2], listemat[3]);
                 scanf("%d",&c2);
-                printf("saisir le numéro de deuxième matrice à additionner(les matrices disponibles sont %s %s %s %s)", listemat[0], listemat[1],listemat[2], listemat[3]);
                 switch(c1){
                     case 0 :
                         switch (c2){
                             case 0 : additionerMatrices(*m0,*m0);
-                                     break;
+                                break;
                             case 1 : additionerMatrices(*m0,*m1);
-                                     break;
+                                break;
                             case 2 : additionerMatrices(*m0,*m2);
-                                     break;
+                                break;
                             case 3 : additionerMatrices(*m0,*m3);
-                                     break;
+                                break;
 
-                            }
+                        }
                     case 1 :
                         switch (c2){
                             case 0 : additionerMatrices(*m1,*m0);
-                                     break;
+                                break;
                             case 1 : additionerMatrices(*m1,*m1);
-                                     break;
+                                break;
                             case 2 : additionerMatrices(*m1,*m2);
-                                     break;
+                                break;
                             case 3 : additionerMatrices(*m1,*m3);
-                                     break;
+                                break;
 
                         }
                     case 2 :
                         switch (c2){
                             case 0 : additionerMatrices(*m2,*m0);
-                                     break;
+                                break;
                             case 1 : additionerMatrices(*m2,*m1);
-                                     break;
+                                break;
                             case 2 : additionerMatrices(*m2,*m2);
-                                     break;
+                                break;
                             case 3 : additionerMatrices(*m2,*m3);
-                                     break;
+                                break;
 
                         }
                     case 3 :
                         switch (c2){
                             case 0 : additionerMatrices(*m0,*m0);
-                                     break;
+                                break;
                             case 1 : additionerMatrices(*m1,*m1);
-                                     break;
+                                break;
                             case 2 : additionerMatrices(*m2,*m2);
-                                     break;
+                                break;
                             case 3 : additionerMatrices(*m3,*m3);
-                                     break;
+                                break;
 
                         }
 
-                    }
-                    break;
+                }
+                break;
 
             case 7 :
-                    printf("saisir le numéro de la matrice dont on souhaite afficher lenombre d'octets gagnés (les matrices disponibles sont %s %s %s %s)", listemat[0], listemat[1],listemat[2], listemat[3]);
-                    int e;
-                    scanf("%d",&e);
-                    switch(e){
-                        case 0: printf("le nombre d'octets gagnés est %d",nombreOctetsGagnes(*m0));break;
-                        case 1: printf("le nombre d'octets gagnés est %d",nombreOctetsGagnes(*m1));break;
-                        case 2: printf("le nombre d'octets gagnés est %d",nombreOctetsGagnes(*m2));break;
-                        case 3: printf("le nombre d'octets gagnés est %d",nombreOctetsGagnes(*m3));break;
-                        default:
-                            printf("saisie incorrecte");
-                            break;
-                    }
-                    break;
+                printf("saisir le numéro de la matrice dont on souhaite afficher le nombre d'octets gagnés (les matrices disponibles sont %s %s %s %s)", listemat[0], listemat[1],listemat[2], listemat[3]);
+                int e;
+                scanf("%d",&e);
+                switch(e){
+                    case 0: printf("le nombre d'octets gagnés est %d",nombreOctetsGagnes(*m0));break;
+                    case 1: printf("le nombre d'octets gagnés est %d",nombreOctetsGagnes(*m1));break;
+                    case 2: printf("le nombre d'octets gagnés est %d",nombreOctetsGagnes(*m2));break;
+                    case 3: printf("le nombre d'octets gagnés est %d",nombreOctetsGagnes(*m3));break;
+                    default:
+                        printf("saisie incorrecte");
+                        break;
+                }
+                break;
 
             case 8: continu = 0;
-                    break;
+                if (listemat[0]!=NULL && listemat[1]==NULL){
+                    libererMatrice(m0);
+                }
+                if (listemat[0]!=NULL && listemat[1]!=NULL && listemat[2]==NULL){
+                    libererMatrice(m0);
+                    libererMatrice(m1);
+                }
+                if (listemat[0]!=NULL && listemat[1]!=NULL && listemat[2]!=NULL && listemat[3]==NULL){
+                    libererMatrice(m0);
+                    libererMatrice(m1);
+                    libererMatrice(m2);
+                }
+                if (listemat[0]!=NULL && listemat[1]!=NULL && listemat[2]!=NULL && listemat[3]!=NULL){
+                    libererMatrice(m0);
+                    libererMatrice(m1);
+                    libererMatrice(m2);
+                    libererMatrice(m3);
+                }
+                break;
+
+            default:
+                printf("Votre entrée n'est pas valide. [1-8]\n");
+                break;
 
         }
-        }
+    }
     return 0;
 }
